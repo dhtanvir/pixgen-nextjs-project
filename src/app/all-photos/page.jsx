6539005fd0f9e71a00a3717ac6d39ad264ebb8de
MD Tanvirul Islam <57485202+dhtanvir@users.noreply.github.com>
2026-04-30 02:30:55 +0600

    category add

--- src/app/all-photos/page.jsx
+++ src/app/all-photos/page.jsx
@@ -1,27 +1,35 @@
 import PhotoCards from "@/components/glary/PhotoCards";
+import Category from "@/components/HomePage/Category";
 import React from "react";
 
-const AllPhotoPage = async () => {
+const AllPhotoPage = async ({ searchParams }) => {
+  const { category } = await searchParams;
+  console.log(category);
+
   const res = await fetch(
     "https://pixgen-nextjs-project-lac.vercel.app/data.json",
   );
   const photos = await res.json();
 
-  // const filteredPhotos = category ? photos.filter(photo => photo.category.toLowerCase() == category.toLowerCase()) : photos
+  const filteredPhotos = category
+    ? photos.filter(
+        (photo) => photo.category.toLowerCase() == category.toLowerCase(),
+      )
+    : photos;
 
   return (
     <div>
       <h1 className="text-2xl font-bold m-4">All Photos</h1>
 
-      {/* <Category/> */}
+      <Category />
 
       <div className="grid grid-cols-4 gap-5">
-        {/* {filteredPhotos.map((photo) => (
-          <PhotoCards key={photo.id} photo={photo} />
-        ))} */}
-        {photos.map((photo) => (
+        {filteredPhotos.map((photo) => (
           <PhotoCards key={photo.id} photo={photo} />
         ))}
+        {/* {photos.map((photo) => (
+          <PhotoCards key={photo.id} photo={photo} />
+        ))} */}
       </div>
     </div>
   );
