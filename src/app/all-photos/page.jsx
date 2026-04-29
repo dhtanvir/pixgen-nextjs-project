353e670e1702a98fae9e91dcfd62e7fdf0a1b519
MD Tanvirul Islam <57485202+dhtanvir@users.noreply.github.com>
2026-04-29 14:53:25 +0600

    all photo add

---
+++ src/app/all-photos/page.jsx
@@ -0,0 +1,30 @@
+import PhotoCards from "@/components/glary/PhotoCards";
+import React from "react";
+
+const AllPhotoPage = async () => {
+  const res = await fetch(
+    "https://pixgen-nextjs-project-lac.vercel.app/data.json",
+  );
+  const photos = await res.json();
+
+  // const filteredPhotos = category ? photos.filter(photo => photo.category.toLowerCase() == category.toLowerCase()) : photos
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold m-4">All Photos</h1>
+
+      {/* <Category/> */}
+
+      <div className="grid grid-cols-4 gap-5">
+        {/* {filteredPhotos.map((photo) => (
+          <PhotoCards key={photo.id} photo={photo} />
+        ))} */}
+        {photos.map((photo) => (
+          <PhotoCards key={photo.id} photo={photo} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AllPhotoPage;
--- src/components/HomePage/TopGeneration.jsx
+++ src/components/HomePage/TopGeneration.jsx
@@ -11,10 +11,10 @@ const TopGeneration = async () => {
 
   return (
     <div className="container mx-auto py-10">
-      <h1 className="text-2xl font-bold text-center">
+      <h1 className="text-2xl font-bold ">
         Top Generation : {photos.length}
       </h1>
-      <div className="grid grid-cols-4 items-center justify-center gap-5 py-10">
+      <div className="grid grid-cols-4 items-center justify-center gap-5 py-5">
         {photos.slice(0,8).map((photo) => (
           <PhotoCards key={photo.id} photo={photo} />
         ))}
